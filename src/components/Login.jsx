6e7e878161dd9e156d24c61cda59f32271907524
sasimopuri <sasikmopuriu@gmail.com>
2025-08-15 22:30:18 +0530

    Added Form Validations

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,27 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { ValidateForm } from "../utils/FormValidations";
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(false);
+  const [error, setError] = useState(null);
   const toggleIsSignIn = () => {
-    setIsSignIn((prevState)=>!prevState);
+    setIsSignIn((prevState) => !prevState);
   };
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const handleFormSubmit = () => {
+    const validation = name
+      ? ValidateForm(
+          name.current.value,
+          email.current.value,
+          password.current.value
+        )
+      : ValidateForm(email.current.value, password.current.value);
+    setError(validation);
+  };
+
   return (
     <>
       <img
@@ -12,19 +29,28 @@ const Login = () => {
         src="https://assets.nflxext.com/ffe/siteui/vlv3/258d0f77-2241-4282-b613-8354a7675d1a/web/IN-en-20250721-TRIFECTA-perspective_cadc8408-df6e-4313-a05d-daa9dcac139f_large.jpg"
         alt=""
       />
-      <form className="absolute bg-black p-12 w-1/4 my-40 mx-auto left-0 right-0 flex flex-col opacity-80 m-2 text-white font-medium">
-        <div className="font-extrabold text-2xl px-2 py-3">{isSignIn? "Sign In" : "Sign Up"}</div>
-        {!isSignIn && <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Name"
-          className="p-2 m-2 bg-slate-700"
-        />}
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute bg-black p-12 w-1/4 my-40 mx-auto left-0 right-0 flex flex-col opacity-80 m-2 text-white font-medium"
+      >
+        <div className="font-extrabold text-2xl px-2 py-3">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </div>
+        {!isSignIn && (
+          <input
+            type="text"
+            name="name"
+            ref={name}
+            id="name"
+            placeholder="Name"
+            className="p-2 m-2 bg-slate-700"
+          />
+        )}
         <input
           type="text"
           name="email"
           id="email"
+          ref={email}
           placeholder="Email or mobile number"
           className="p-2 m-2 bg-slate-700"
         />
@@ -32,24 +58,37 @@ const Login = () => {
           type="password"
           name="password"
           id="password"
+          ref={password}
           placeholder="Password"
           className="p-2 m-2 bg-slate-700"
         />
-        <button className="m-2 p-2 bg-red-700 opacity-100">
+        <p className="px-2 text-red-600">{error}</p>
+        <button
+          className="m-2 p-2 bg-red-700 opacity-100"
+          onClick={handleFormSubmit}
+        >
           {isSignIn ? "Sign In" : "Sign Up"}
         </button>
         {isSignIn ? (
           <div className="p-2 font-medium">
             {" "}
             Not registred yet?{" "}
-            <span className="cursor-pointer font-extrabold" onClick={toggleIsSignIn}>Sign Up</span>
+            <span
+              className="cursor-pointer font-extrabold"
+              onClick={toggleIsSignIn}
+            >
+              Sign Up
+            </span>
           </div>
         ) : (
-          
-
           <div className="p-2 font-medium">
             Alredy registred?{" "}
-            <span className="cursor-pointer font-extrabold" onClick={toggleIsSignIn}>Sign In</span>
+            <span
+              className="cursor-pointer font-extrabold"
+              onClick={toggleIsSignIn}
+            >
+              Sign In
+            </span>
           </div>
         )}
       </form>
---
+++ src/utils/FormValidations.js
@@ -0,0 +1,18 @@
+export const ValidateForm = (...args) => {
+  let name, email, password;
+  if (args.length === 3) {
+    [name, email, password] = args;
+    console.log("inside validate", name, email, password);
+  } else {
+    [email, password] = args;
+  }
+  const isNameValid = name && /^[A-Za-z\s]+$/.test(name);
+  const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
+  const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+
+  if (name && !isNameValid) return "Name is not valid";
+  if (!isEmailValid) return "Email is not valid";
+  if (!isPasswordValid) return "Password is not valid";
+
+  return null;
+};
