46aac62382b245038256a42dd468a90e5da141d1
sasimopuri <sasikmopuriu@gmail.com>
2025-08-16 10:06:01 +0530

    Fixed minor bugs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
   const email = useRef(null);
   const password = useRef(null);
   const handleFormSubmit = () => {
-    const validation = name
+    const validation = !isSignIn
       ? ValidateForm(
           name.current.value,
           email.current.value,
@@ -25,9 +25,9 @@ const Login = () => {
   return (
     <>
       <img
-        className="absolute"
+        className="absolute w-screen h-screen object-cover"
         src="https://assets.nflxext.com/ffe/siteui/vlv3/258d0f77-2241-4282-b613-8354a7675d1a/web/IN-en-20250721-TRIFECTA-perspective_cadc8408-df6e-4313-a05d-daa9dcac139f_large.jpg"
-        alt=""
+        alt="background"
       />
       <form
         onSubmit={(e) => e.preventDefault()}
