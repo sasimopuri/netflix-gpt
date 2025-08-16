fa239792959d3bfb19170309e4599530e49b6ac9
sasimopuri <sasikmopuriu@gmail.com>
2025-08-16 19:28:32 +0530

    Configured redux tool kit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { Provider } from "react-redux";
 import Body from "./components/Body";
 import Header from "./components/Header";
 import Login from "./components/Login";
+import appStore from "./utils/appStore";
 
 function App() {
   return (
-    <>
+    <Provider store={appStore}>
       <Header />
       <Body />
-    </>
+    </Provider>
   );
 }
 export default App;
