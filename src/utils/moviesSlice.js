b1d13b0ab14a7212374e76df78fb8b0e6263d2ac
sasimopuri <sasikmopuriu@gmail.com>
2025-08-20 10:40:20 +0530

    Added Video Deatils in movie Slice

--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -4,13 +4,17 @@ const movieSlice = createSlice({
   name: "movies",
   initialState: {
     nowPlaying: null,
+    videoDetails: null,
   },
   reducers: {
     addMovies: (state, action) => {
       state.nowPlaying = action.payload;
     },
+    addVideoDetails: (state, action) => {
+      state.videoDetails = action.payload;
+    },
   },
 });
 
-export default movieSlice.reducer
-export const {addMovies} = movieSlice.actions
+export default movieSlice.reducer;
+export const { addMovies, addVideoDetails } = movieSlice.actions;
