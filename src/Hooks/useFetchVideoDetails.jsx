8b80cb09070f93704d80f2cd02d1365a37dd635b
sasimopuri <sasikmopuriu@gmail.com>
2025-08-22 00:29:02 +0530

    Made details dynamic

--- src/Hooks/useFetchVideoDetails.jsx
+++ src/Hooks/useFetchVideoDetails.jsx
@@ -3,11 +3,11 @@ import { TMDB_API_OPTIONS } from "../utils/constants";
 import { useDispatch } from "react-redux";
 import { addVideoDetails } from "../utils/moviesSlice";
 
-const useFetchVideoDetails = () => {
+const useFetchVideoDetails = (movieId) => {
   const dispatch = useDispatch();
   const fetchVideoDetails = async () => {
     const response = await fetch(
-      "https://api.themoviedb.org/3/movie/755898/videos?language=en-US",
+      `https://api.themoviedb.org/3/movie/+${movieId}+/videos?language=en-US`,
       TMDB_API_OPTIONS
     );
     const data = await response.json();
